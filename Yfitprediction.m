e0bff34994f21ae406193bbeb0658a06e954dd62
Saeid <s.saeid.tayebi@gmail.com>
2024-08-22 19:10:53 -0400

    Function files fixed

--- Yfitprediction.m
+++
@@ -1,9 +0,0 @@
- function [y_hat,t_score]=Yfitprediction(pls_model,x_pre)
-
-        %%% Receive the right PLS model and X_new( unscaled and un
-        %%% centered) calculate the corresponding Y-fit and unscale it
-        [x_pre,~]=scaler(pls_model,x_pre,0);
-        t_score=x_pre*pls_model.Wstar;
-        y_hat=x_pre*pls_model.B_pls;        
-        [~,y_hat]=Unscaler(pls_model,0,y_hat);
-    end
